910df41722056081ff7e02a09daefb3a110f5176
pkc56 <93231721+pkc56@users.noreply.github.com>
2021-12-06 22:49:49 -0600

    Create index.js
    forum doesnt  highlight red when forum is filled out incorrectly

---
+++ index.js
@@ -0,0 +1,68 @@
+const form = document.getElementbyId("form");
+const username = document.getElementbyId("Username");
+const email = document.getElementbyId("Email");
+const password = document.getElementById("Password");
+const password2 = document.getElementById("Password2");
+
+// Event listeners
+form.addEventListener("submit", function(e)  {
+    e.preventDefault();
+// console.log(username.value);   <-- checking if wokring properly
+    if (username.value  === "") 
+    {
+        showError(username, "Username must be filled out");
+    } else 
+    {
+        showSuccess(username);
+    }
+
+    if (email.value === "") 
+    {
+        showError(email, "Email must be filled out");
+    } else if (!isValidEmail(email.value)) 
+    {
+        showError(email, "Email is not a valid email");
+    } else
+    {
+        showSuccess(email);
+    }
+
+    if(password.value === "")    
+    {
+        showError(password, "Password is required");
+    } else
+    {
+        showSuccess(password);
+    }
+    if (password2.value === password || password2.value === "")
+    {
+        showError(password2, "Passwords do not match") ;
+    } else 
+    {
+        showSuccess(password2);
+    }
+})
+
+//Show outline for error input
+function showError(input, message) 
+{
+    const formControl = input.parentElement;
+    formControl.classname = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+// Showw outline for success input
+const showSuccess = (input) => 
+{
+const formControl = input.parentElement;
+formControl.className = "form-control success";
+}
+
+// Check if valid email
+const isValidEmail = (email) =>
+{
+    //Search google for regualr expression in javascript
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(String(email).toLowerCase())
+}
